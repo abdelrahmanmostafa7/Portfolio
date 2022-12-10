333d53b22e6dc596580157364d472ee50e86061e
boyka <abdelrahmanzaki747@gmail.com>
2022-12-10 06:44:43 +0200

    first commit

---
+++ my-app/src/components/intro/Intro.jsx
@@ -0,0 +1,24 @@
+import "./intro.scss"
+export default function Intro() {
+  
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="" />
+        </div>
+      </div>
+
+      <div className="right">
+        <div className="wrapper">
+            <h2>Hi there, I'm</h2>
+            <h1>Abdelrahman Mostafa</h1>
+          <h3>Software Engineer</h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
+    </div>
+  )
+}
